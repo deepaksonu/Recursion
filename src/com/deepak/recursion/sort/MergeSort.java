275887695079aed74a920ed0639b41d77c1e31d3
deepaksonu <deepaksonu16@gmail.com>
2021-11-10 16:23:24 +0530

    new recursion problem.

---
+++ src/com/deepak/recursion/sort/MergeSort.java
@@ -0,0 +1,100 @@
+package com.deepak.recursion.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+
+        int[] arr = {8,3,4,12,5,6};
+        //System.out.println(Arrays.toString(mergeSort(arr)));
+        mergeSortInPlace(arr,0,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] mergeSort(int[] arr){
+
+        if(arr.length == 1){
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] arr = new int[left.length + right.length];
+
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                arr[k] = left[i];
+                i++;
+            }else{
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left.length){
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j < right.length){
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+        return arr;
+    }
+
+
+    static void mergeSortInPlace(int[] arr, int s, int e){
+        if(e - s == 1) {
+            return;
+        }
+        int mid = s + (e-s)/2;
+        mergeSortInPlace(arr,s,mid);
+        mergeSortInPlace(arr,mid,e);
+
+        mergeInPlace(arr,s,mid,e);
+    }
+
+    static void mergeInPlace(int[] arr,int s, int m, int e){
+        int i = 0;
+        int j = m;
+        int k = 0;
+        int[] mix = new int[e];
+        while(i < s && j < e){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }else{
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < s){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j < e){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int l = 0; i < arr.length ; i++){
+            arr[l] = mix[l];
+        }
+    }
+
+}
