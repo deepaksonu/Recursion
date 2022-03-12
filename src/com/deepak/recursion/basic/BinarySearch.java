f209592a12fa741448eb885690f98b94b6c4efc4
deepaksonu <deepaksonu16@gmail.com>
2022-03-12 12:39:58 +0530

    new recursion problem.

--- src/com/deepak/recursion/basic/BinarySearch.java
+++ src/com/deepak/recursion/basic/BinarySearch.java
@@ -1,9 +1,62 @@
 package com.deepak.recursion.basic;
 
+/**
+ *
+ 0 1 2 3  4  5  6  7
+ arr = 1 2 3 44 56 67 86 98
+ target = 86
+
+ static int binarySearch(int[] arr,int target,int s,int e){
+ if(s > e){
+ return -1;
+ }
+
+ int mid = s + (e-s)/2;
+ if(target < arr[mid]){
+ return binarySearch(arr,target,s,mid-1);
+ }else if(target > arr[mid]){
+ return binarySearch(arr,target,mid+1,e);
+ }else
+ return mid;
+
+ }
+
+ target = 86
+ -> arr,86,0,7 ; m = 4
+ -> arr,86,5,7 ; m = 6
+ return 6
+
+ target =2
+ -> arr,2,0,7 ; m = 4
+ -> arr,2,0,3 ; m = 2
+ -> arr,2,0,1 ; m = 1
+ return 1
+
+ target = 98
+ -> arr,98,0,7 ; m = 4
+ -> arr,98,5,7 ; m = 6
+ return 6
+
+ target = 102
+ -> arr,102,0,7 ; m = 4
+ -> arr,102,5,7 ; m = 6
+ -> arr,102,7,7 ; m = 7
+ -> arr,102,8,7 ;
+ return -1
+
+ target =1
+ -> arr,1,0,7 ; m = 4
+ -> arr,1,0,3 ; m = 2
+ -> arr,1,0,1 ; m = 1
+ -> arr,1,0,0 ;
+ return 0
+
+ */
 public class BinarySearch  {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,44,56, 78,86};
+        System.out.println(arr.length);
         int target = 86;
         System.out.println(search(arr,target,0, arr.length-1));
     }
--- src/com/deepak/recursion/basic/ReverseNumber.java
+++ src/com/deepak/recursion/basic/ReverseNumber.java
@@ -3,9 +3,10 @@ package com.deepak.recursion.basic;
 public class ReverseNumber {
 
     public static void main(String[] args) {
-
+        System.out.println(Math.log10(1234));
         System.out.println(reverse(1234));
         System.out.println(isPallindrome(1234));
+        System.out.println(reverseNum(1234,0));
     }
 
     static int reverse(int n){
@@ -22,6 +23,11 @@ public class ReverseNumber {
         return (int) ((n%10) * Math.pow(10,digits -1 ) + helper(n/10, digits -1));
     }
 
+    static int reverseNum(int n,int revN){
+        if(n ==0) return revN;
+
+        return reverseNum(n/10, revN*10 + n%10);
+    }
     static boolean isPallindrome(int n){
         return n == reverse(n);
     }
